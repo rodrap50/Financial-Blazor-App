d3aaf60b15ed7eb63bd4b94d7f242e931f76f340
JustTheSuff <SuperSuff@yahoo.com>
2020-10-29 23:10:24 -0400

    First setup configuration for the application.

---
+++ FinancialSetup/FinancialEntry.cs
@@ -0,0 +1,36 @@
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System;
+
+namespace FinancialSetup
+{
+
+    public class FinancialEntry
+    {
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public string TransactionNumber { get; set; }
+        public string AccountId { get; set; }
+        public string EventId { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public FinancialEntryType EntryType { get; set; }
+        public string CheckNumber { get; set; }
+    }
+
+    public enum FinancialEntryType
+    {
+        [EnumMember(Value = "cash")]
+        Cash,
+        [EnumMember(Value = "check")]
+        Check,
+        [EnumMember(Value = "transfer")]
+        Transfer,
+        [EnumMember(Value = "electronic")]
+        Electronic
+    }
+
+
+}
