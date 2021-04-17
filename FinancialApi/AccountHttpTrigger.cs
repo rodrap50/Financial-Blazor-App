8a62fa1c66ca9d5f7536c898fcc313cba549b002
JustTheSuff <supersuff@yahoo.com>
2021-04-16 23:21:37 -0400

    Update account transaction summaries

--- FinancialApi/AccountHttpTrigger.cs
+++ FinancialApi/AccountHttpTrigger.cs
@@ -209,7 +209,7 @@ namespace Rodrap50.Financial.Api
 
             Document document = await client.CreateDocumentAsync("/dbs/Rodrap50/colls/Financials/", transaction);
 
-            StoredProcedureResponse<AccountResponse> sprocResponse2 = await client.ExecuteStoredProcedureAsync<AccountResponse>(
+            StoredProcedureResponse<AccountResponse[]> sprocResponse2 = await client.ExecuteStoredProcedureAsync<AccountResponse[]>(
                                                                 "/dbs/Rodrap50/colls/Financials/sprocs/AddAccountTransaction/", new RequestOptions { PartitionKey = new PartitionKey("account") }, transaction);
 
 
--- FinancialApi/CosmosDb/StoredProcedures/AddAccountTransaction.js
+++ FinancialApi/CosmosDb/StoredProcedures/AddAccountTransaction.js
@@ -12,6 +12,8 @@ function AddAccountTransaction(transaction) {
 
     if (!transaction.fromAccountId && !transaction.toAccountId && !transaction.generalAccountId) throw new Error("[ERR-AR5005] The transaction must be associated to at least one account id.");
 
+    var accounts = [];
+
     if (transaction.generalAccountId) {
         var filterQuery =
         {
@@ -60,17 +62,132 @@ function AddAccountTransaction(transaction) {
                 trans_summary.amount = trans_entry.amount;
                 account_record.transactionSummary.push(trans_summary);
                 
+                accounts.push(account_record);
+
                 // Update the account information
                 var accept2 = container.replaceDocument(account_record._self, account_record,
                     function (err, itemReplaced) {
                         if (err) throw "[ERR-AR1211] Unable to update the account document.";
                     });
             });
+        if (!accept) throw "[ERR-AR1212] Unable to update the account document.";
+    }
 
+    if (transaction.fromAccountId) {
+        var filterQuery =
+        {
+            'query': 'SELECT * FROM Financials p where p.id = @accountid and p.recordCode = "account"',
+            'parameters': [{ 'name': '@accountid', 'value': transaction.fromAccountId }]
+        };
+
+        var accept = container.queryDocuments(container.getSelfLink(), filterQuery, {},
+            function (err, items, responseOptions) {
+                if (err) throw new Error("Error" + err.message);
+
+                if (items.length != 1) throw "[ERR-AR1210] Account document not found.";
+                account_record = items[0];
+
+                if (!account_record) throw new Error("[ERR-AR1210] Account document not found.");
+
+                // Add the transaction to the ledger list
+                var trans_entry = {};
+                trans_entry.id = transaction.id;
+              
+                trans_entry.amount = transaction.direction = 'credit' ? (transaction.amount * -1) : transaction.amount;
+                
+                if (!account_record.transactions) { account_record.transactions = []; }
+                account_record.transactions.push(trans_entry);
+
+                // Calculate the account balance from the ledger list
+                account_record.balance = 0;
+                account_record.transactions.forEach(function (item, index, obj) {
+                    account_record.balance = account_record.balance + item.amount;
+                });
+
+                // Update the summary and remove if over max transaction summary
+                if (!account_record.transactionSummary) { account_record.transactionSummary = []; }
+
+                if(account_record.transactionSummary.length >= MAX_TRANSACTION_SUMMARY) {
+                    account_record.transactionSummary.shift();
+                }
+                
+                var trans_summary = {};
+                trans_summary.id = transaction.id;
+                trans_summary.recordId = transaction.recordId;
+                trans_summary.dateIncurred = transaction.dateIncurred;
+                trans_summary.description = transaction.description;
+                trans_summary.amount = trans_entry.amount;
+                account_record.transactionSummary.push(trans_summary);
+
+                accounts.push(account_record);
+                
+                // Update the account information
+                var accept2 = container.replaceDocument(account_record._self, account_record,
+                    function (err, itemReplaced) {
+                        if (err) throw "[ERR-AR1211] Unable to update the account document.";
+                    });
+            });
+        if (!accept) throw "[ERR-AR1212] Unable to update the account document.";
     }
     // Query for the From Account Document
 
+    if (transaction.toAccountId) {
+        var filterQuery =
+        {
+            'query': 'SELECT * FROM Financials p where p.id = @accountid and p.recordCode = "account"',
+            'parameters': [{ 'name': '@accountid', 'value': transaction.toAccountId }]
+        };
 
-    if (!accept) throw "[ERR-AR1212] Unable to update the account document.";
+        var accept = container.queryDocuments(container.getSelfLink(), filterQuery, {},
+            function (err, items, responseOptions) {
+                if (err) throw new Error("Error" + err.message);
 
+                if (items.length != 1) throw "[ERR-AR1210] Account document not found.";
+                account_record = items[0];
+
+                if (!account_record) throw new Error("[ERR-AR1210] Account document not found.");
+
+                // Add the transaction to the ledger list
+                var trans_entry = {};
+                trans_entry.id = transaction.id;
+              
+                trans_entry.amount = transaction.direction = 'credit' ? transaction.amount : (transaction.amount * -1);
+                
+                if (!account_record.transactions) { account_record.transactions = []; }
+                account_record.transactions.push(trans_entry);
+
+                // Calculate the account balance from the ledger list
+                account_record.balance = 0;
+                account_record.transactions.forEach(function (item, index, obj) {
+                    account_record.balance = account_record.balance + item.amount;
+                });
+
+                // Update the summary and remove if over max transaction summary
+                if (!account_record.transactionSummary) { account_record.transactionSummary = []; }
+
+                if(account_record.transactionSummary.length >= MAX_TRANSACTION_SUMMARY) {
+                    account_record.transactionSummary.shift();
+                }
+                
+                var trans_summary = {};
+                trans_summary.id = transaction.id;
+                trans_summary.recordId = transaction.recordId;
+                trans_summary.dateIncurred = transaction.dateIncurred;
+                trans_summary.description = transaction.description;
+                trans_summary.amount = trans_entry.amount;
+                account_record.transactionSummary.push(trans_summary);
+
+                accounts.push(account_record);
+                
+                // Update the account information
+                var accept2 = container.replaceDocument(account_record._self, account_record,
+                    function (err, itemReplaced) {
+                        if (err) throw "[ERR-AR1211] Unable to update the account document.";
+                    });
+            });
+            if (!accept) throw "[ERR-AR1212] Unable to update the account document.";
+    }
+
+    getContext().getResponse().setBody(accounts);
+   
 }
